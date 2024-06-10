6b8bfeeae04ca7640bdafaac1e442c4868311bdf
gastawny <gastawny@gmail.com>
2024-06-09 21:04:12 -0300

    Add subnet calc

---
+++ src/app/api/subnet/route.ts
@@ -0,0 +1,74 @@
+interface SubnetInfo {
+  subnetID: number;
+  subnetAddress: string;
+  hostAddressRange: {
+    start: string;
+    end: string;
+  };
+  notation: string;
+}
+
+export async function GET(req: Request) {
+  const [{ block }, { nSubnets }] = req.url
+    .split("?")[1]
+    .split("&")
+    .map((x) => {
+      const [key, value] = x.split("=");
+
+      return {
+        [key]: value,
+      };
+    }) as any;
+
+  const dec2hex = (decimal: number) => decimal.toString(16);
+
+  const concatHex = (hex1: string, hex2: string) =>
+    hex1.slice(0, -hex2.length) + hex2;
+
+  const format2IPv6 = (hex: string) => hex.match(/.{1,4}/g)!.join(":");
+
+  const abbreviateIPv6 = (ip: string) => {
+    let ipArr = ip.split(":").filter((x) => x !== "");
+
+    if (Number(ipArr[3]) == 0) ipArr = ipArr.slice(0, 3);
+    if (Number(ipArr[2]) == 0 && ipArr.length == 3) ipArr = ipArr.slice(0, 2);
+    if (Number(ipArr[1]) == 0 && ipArr.length == 2) ipArr = ipArr.slice(0, 1);
+
+    if (ipArr[ipArr.length - 1][0] == "0")
+      ipArr[ipArr.length - 1] = ipArr[ipArr.length - 1].replace(/^0+/, "");
+
+    return ipArr.join(":") + "::";
+  };
+
+  function subnetCalculator(block: string, subnets: number) {
+    const [blockStr, blockNumStr] = block.split("/");
+    const blockNum = parseInt(blockNumStr);
+
+    const subnetsArr: SubnetInfo[] = [];
+    const maxSubnets = Math.pow(2, 64 - blockNum - subnets);
+
+    const initialSubnet = blockStr.split("::")[0] + "000000000000";
+
+    for (let i = 0; i < Math.pow(2, subnets); i++) {
+      const base =
+        format2IPv6(concatHex(initialSubnet, dec2hex(maxSubnets * i))) + "::";
+
+      subnetsArr.push({
+        subnetID: i + 1,
+        subnetAddress: abbreviateIPv6(base),
+        hostAddressRange: {
+          start: abbreviateIPv6(base),
+          end: abbreviateIPv6(
+            format2IPv6(
+              concatHex(initialSubnet, dec2hex(maxSubnets * (i + 1) - 1))
+            )
+          ),
+        },
+        notation: `${abbreviateIPv6(base)}/${blockNum + subnets}`,
+      });
+    }
+
+    return subnetsArr;
+  }
+  return Response.json(subnetCalculator(block, Number(nSubnets)));
+}
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -12,26 +12,26 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { memo } from "react";
 
 function MemoizedTable({ data }: { data: any }) {
-  console.log(data);
-
   return (
-    <ScrollArea className="h-[20rem] 2xl:h-[28rem]">
+    <ScrollArea className="h-[22rem] 2xl:h-[28rem]">
       <Table>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[100px]">Subnet ID</TableHead>
+            <TableHead className="w-24">Subnet ID</TableHead>
             <TableHead>Subnet Address </TableHead>
             <TableHead>Host Address Range </TableHead>
-            <TableHead className="text-right">Notation</TableHead>
+            <TableHead>Notation</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {data.map((d: any, i: number) => (
             <TableRow key={i}>
-              <TableCell className="font-medium">{i + 1}</TableCell>
-              <TableCell>{d?.paymentStatus}</TableCell>
-              <TableCell>{d?.paymentMethod}</TableCell>
-              <TableCell className="text-right">{d?.totalAmount}</TableCell>
+              <TableCell className="font-medium">{d.subnetID}</TableCell>
+              <TableCell>{d.subnetAddress}</TableCell>
+              <TableCell>
+                {d.hostAddressRange.start} - {d.hostAddressRange.end}
+              </TableCell>
+              <TableCell>{d.notation}</TableCell>
             </TableRow>
           ))}
         </TableBody>
